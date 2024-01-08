018e5f3009423f51448e7c5513da781afa5f771a
Ilan Uzan <ilan9uzan@gmail.com>
2024-01-08 22:03:51 +0200

    add flutter dockerfile of the repo (will be used for tests) (#11)
    Co-authored-by: Ilan Uzan <ilanuzan@Ilans-MacBook-Air.local>

--- scripts/convert_tileset.dart
+++ scripts/convert_tileset.dart
@@ -1,54 +1,17 @@
+import 'package:path/path.dart' as p;
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:path/path.dart';
-
 const debugMode = true;
 const tiles = [
-  "TILE_1",
-  "TILE_1_SEL",
-  "CHARACTER_1",
-  "CHARACTER_2",
-  "CHARACTER_3",
-  "CHARACTER_4",
-  "CHARACTER_5",
-  "CHARACTER_6",
-  "CHARACTER_7",
-  "CHARACTER_8",
-  "CHARACTER_9",
-  "BAMBOO_1",
-  "BAMBOO_2",
-  "BAMBOO_3",
-  "BAMBOO_4",
-  "BAMBOO_5",
-  "BAMBOO_6",
-  "BAMBOO_7",
-  "BAMBOO_8",
-  "BAMBOO_9",
-  "ROD_1",
-  "ROD_2",
-  "ROD_3",
-  "ROD_4",
-  "ROD_5",
-  "ROD_6",
-  "ROD_7",
-  "ROD_8",
-  "ROD_9",
-  "SEASON_1",
-  "SEASON_2",
-  "SEASON_3",
-  "SEASON_4",
-  "FLOWER_1",
-  "FLOWER_2",
-  "FLOWER_3",
-  "FLOWER_4",
-  "WIND_1",
-  "WIND_2",
-  "WIND_3",
-  "WIND_4",
-  "DRAGON_1",
-  "DRAGON_2",
-  "DRAGON_3"
+  "TILE_1", "TILE_1_SEL",
+  "CHARACTER_1", "CHARACTER_2", "CHARACTER_3", "CHARACTER_4", "CHARACTER_5", "CHARACTER_6", "CHARACTER_7", "CHARACTER_8", "CHARACTER_9",
+  "BAMBOO_1", "BAMBOO_2", "BAMBOO_3", "BAMBOO_4", "BAMBOO_5", "BAMBOO_6", "BAMBOO_7", "BAMBOO_8", "BAMBOO_9",
+  "ROD_1", "ROD_2", "ROD_3", "ROD_4", "ROD_5", "ROD_6", "ROD_7", "ROD_8", "ROD_9",
+  "SEASON_1", "SEASON_2", "SEASON_3", "SEASON_4",
+  "FLOWER_1", "FLOWER_2", "FLOWER_3", "FLOWER_4",
+  "WIND_1", "WIND_2", "WIND_3", "WIND_4",
+  "DRAGON_1", "DRAGON_2", "DRAGON_3"
 ];
 
 const tilesetPath = "./assets/tilesets/";
@@ -63,9 +26,9 @@ void main(List<String> tilesets) async {
   if (tilesets.length == 0) {
     tilesets = [];
     await for (var file in _listFilesInDirectory(tilesetPath)) {
-      final base = basename(file.path);
+      final base = p.basename(file.path);
       if (!base.endsWith(".desktop")) continue;
-      tilesets.add(basenameWithoutExtension(file.path));
+      tilesets.add(p.basenameWithoutExtension(file.path));
     }
   }
   debug("tilesets: ${tilesets.join(', ')}");
@@ -74,9 +37,11 @@ void main(List<String> tilesets) async {
   }
 }
 
-Stream<FileSystemEntity> _listFilesInDirectory(String path) {
-  debug("Listing files in directory `$path`..");
-  return Directory.fromRawPath(Uint8List.fromList(canonicalize(path).codeUnits)).list();
+Stream<FileSystemEntity> _listFilesInDirectory(String directoryPath) {
+  debug("Listing files in directory `$directoryPath`..");
+  return Directory.fromRawPath(Uint8List.fromList(
+      p.canonicalize(directoryPath).codeUnits)
+  ).list();
 }
 
 Future<void> _runCommand(String executable, List<String> arguments) async {
@@ -103,7 +68,7 @@ Future<void> _pngfyTileset(String tileset) async {
       "--export-type=png",
       "--export-id=$tile",
       "--export-filename=${target + tile}.png",
-      absolute(canonicalize("$tilesetPath$svgFilename"))
+      p.absolute(p.canonicalize("$tilesetPath$svgFilename"))
     ]);
   }));
 }
